e36d4fe9ddba2664cfee2ebb33b42d7ed09beef5
sinenlu <sinen0108@gmail.com>
2018-07-20 14:55:03 +0800

    Article

--- models/article_model.js
+++ models/article_model.js
@@ -9,6 +9,11 @@ let articleSchema = new Schema({
   Images: Object,
   Date: String,
   Location : { type: {type:String}, coordinates: [Number]},
+  Likes: Number,
+  LikesPeople: String,
+  CommentPerson: String,
+  CommentContent: String,
+  CommentTime: String
 }, { versionKey: false });
 
 articleSchema.index({loc: '2dsphere'});
